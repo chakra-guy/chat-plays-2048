04bfedb3226ca60d67e42b041b8b3097efab9f89
Tamas Soos <soostamas.hu@gmail.com>
2020-01-25 12:58:22 +0000

    Fixing eslint issues

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,31 +1,32 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ["airbnb", "react-app", "prettier", "prettier/react"],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react", "react-hooks", "prettier"],
   rules: {
     "prettier/prettier": ["error"],
-    "react-hooks/rules-of-hooks": "error"
+    "react-hooks/rules-of-hooks": "error",
   },
   overrides: [
     {
       files: ["*.test.js"],
       rules: {
-        "react/jsx-props-no-spreading": "off"
-      }
-    }
-  ]
-};
+        "react/jsx-props-no-spreading": "off",
+        "react/jsx-filename-extension": "off",
+      },
+    },
+  ],
+}
