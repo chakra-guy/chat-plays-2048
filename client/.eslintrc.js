75ec678af007a55bc50ab7fc6c0d640b3b068e59
Tamas Soos <soostamas.hu@gmail.com>
2020-01-25 12:53:58 +0000

    Setting up ESLint

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb", "react-app", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "react-hooks/rules-of-hooks": "error"
+  },
+  overrides: [
+    {
+      files: ["*.test.js"],
+      rules: {
+        "react/jsx-props-no-spreading": "off"
+      }
+    }
+  ]
+};
