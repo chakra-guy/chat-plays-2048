fe0862ce96baa461bb4c4cd2a975ff91b131e789
Tamas Soos <soostamas.hu@gmail.com>
2020-01-28 19:50:42 +0000

    Refactoring redux-middleware

--- client/src/_websocket/redux-middleware.js
+++ client/src/_websocket/redux-middleware.js
@@ -1,46 +1,21 @@
-import { Socket, Presence } from "phoenix"
+import { Socket } from "phoenix"
 
 import {
-  SETUP_WEBSOCKET,
-  JOIN_CHANNEL,
-  joinChannelSuccess,
-  joinChannelError,
-} from "./actions"
-import {
-  updateMessageList,
-  updateOnlineUsers,
-  SEND_NEW_MESSAGE,
-} from "../Chat/actions"
-import {
-  initializeGame,
-  gameMoved,
-  gameVoting,
-  gameRestarted,
-  gameModeChanged,
-  MAKE_MOVE,
-  RESTART_GAME,
-  CHANGE_GAME_MODE,
-} from "../Game/actions"
+  joinChannel,
+  handleGameChannelMessages,
+  handleChatChannelMessages,
+} from "./utils"
+import { SETUP_WEBSOCKET, JOIN_CHANNEL } from "./actions"
+import { SEND_NEW_MESSAGE } from "../Chat/actions"
+import { MAKE_MOVE, RESTART_GAME, CHANGE_GAME_MODE } from "../Game/actions"
 
 const WS_URL = process.env.REACT_APP_WS_URL
 
-function joinChannel(socket, topic, dispatch) {
-  const channel = socket.channel(topic)
-
-  channel
-    .join()
-    .receive("ok", () => dispatch(joinChannelSuccess()))
-    .receive("error", () => dispatch(joinChannelError()))
-
-  return channel
-}
-
-export default function websocketMiddleware({ dispatch, getState }) {
+export default function websocketMiddleware({ dispatch }) {
   let socket = null
-  let presences = {}
   const channels = {}
 
-  return next => async action => {
+  return next => action => {
     const { payload, type } = action
 
     switch (type) {
@@ -53,37 +28,10 @@ export default function websocketMiddleware({ dispatch, getState }) {
         channels[payload.name] = joinChannel(socket, payload.topic, dispatch)
 
         if (payload.name === "game") {
-          channels.game.on("game:init", response => {
-            dispatch(initializeGame(response))
-          })
-          channels.game.on("game:moved", response => {
-            dispatch(gameMoved(response))
-          })
-          channels.game.on("game:voting", response => {
-            dispatch(gameVoting(response))
-          })
-          channels.game.on("game:restarted", response => {
-            dispatch(gameRestarted(response))
-          })
-          channels.game.on("game:game_mode_changed", response => {
-            dispatch(gameModeChanged(response))
-          })
+          handleGameChannelMessages(channels, dispatch)
         }
-
         if (payload.name === "chat") {
-          channels.chat.on("chat:new_msg", res => {
-            dispatch(updateMessageList(res))
-          })
-          channels.chat.on("presence_state", response => {
-            presences = Presence.syncState(presences, response)
-            const onlineUsers = Presence.list(presences).map(p => p.metas[0])
-            dispatch(updateOnlineUsers(onlineUsers))
-          })
-          channels.chat.on("presence_diff", res => {
-            presences = Presence.syncDiff(presences, res)
-            const onlineUsers = Presence.list(presences).map(p => p.metas[0])
-            dispatch(updateOnlineUsers(onlineUsers))
-          })
+          handleChatChannelMessages(channels, dispatch)
         }
         break
 
