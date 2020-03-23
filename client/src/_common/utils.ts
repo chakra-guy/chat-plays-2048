0070ef7cafe8b80a02b61c88383b681d40a47e80
Tamas Soos <soostamas.hu@gmail.com>
2020-03-23 22:59:33 +0000

    Fixing utils

--- client/src/_common/utils.ts
+++ client/src/_common/utils.ts
@@ -1,23 +1,24 @@
 import generateName from "sillyname"
 import moment from "moment"
 import attempt from "lodash.attempt"
-import isError from "lodash.iserror"
 
 const STORAGE_KEY = "ChatPlays2048_USERNAME"
 
 export function setupRandomUsername(): string {
-  let username: string | Error
+  let username: string
 
-  username = attempt(() =>
-    JSON.parse(localStorage.getItem(STORAGE_KEY) as string),
-  )
+  username = attempt(() => {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) as string)
+  })
 
-  if (isError(username)) {
+  if (!username) {
     username = generateName()
       .replace(" ", "_")
       .toLowerCase()
 
-    attempt(() => localStorage.setItem(STORAGE_KEY, JSON.stringify(username)))
+    attempt(() => {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(username))
+    })
   }
 
   return username
